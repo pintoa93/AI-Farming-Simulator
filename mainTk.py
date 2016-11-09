1c31841f3307b3be61fb72740576b3d33800e308
kamilrzesz <rzeszotk@uni.coventry.ac.uk>
2016-11-09 15:15:58 +0000

    Update mainTk.py

--- mainTk.py
+++ mainTk.py
@@ -1,5 +1,9 @@
 from tkinter import *
 import time
+def move_widget(item, x, y):
+    ### Moves a widget(item) on the main canvas to the new coordinates(x,y)###
+    main_canvas.coords(tractor1,x,y)
+    main_canvas.update()
 root = Tk()
 icon = Image("photo", file="tractor_image.png")
 root.tk.call('wm','iconphoto',root._w,icon)
@@ -10,9 +14,6 @@ tractor1 = main_canvas.create_image(0,0,image=tractor_img, anchor = NW)
 t1,t2= main_canvas.coords(tractor1)
 print(t1,t2)
 for i in range(0,600,1):
-    main_canvas.coords(tractor1,t1+i,t2+10)
-    main_canvas.update()
+    move_widget(tractor1, t1+i,t2)
     time.sleep(0.01)
 mainloop()
-
-
